21258fb7139f5368dd1b7daa863019c7a53d35de
Dale Bendixen <dalebendixen@gmail.com>
2023-06-13 16:01:57 -0400

    added try catch block

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,12 +2,17 @@ const Users = require('../models/userModel');
 
 
 const CreateUser = async (req, res) => {
-    const {userName, firstName, lastName, email, password} = req.body;
-    const newUser = new Users({
-        userName, firstName, lastName, email, password
-    });
-    await newUser.save();
-    res.status(201).json({msg: 'User Created', data: newUser});
+    try {
+        const {userName, firstName, lastName, email, password, } = req.body;
+        const newUser = new Users({
+            userName, firstName, lastName, email, password, 
+        });
+        await newUser.save();
+        res.status(201).json({msg: 'User Created', data: newUser});
+        
+    } catch (error) {
+        res.status(200).json({msg:error})
+    }
 };
 
 module.exports = {
