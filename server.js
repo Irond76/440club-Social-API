34321b9a1844245b0ccda9307d664da221421a6f
Dale Bendixen <dalebendixen@gmail.com>
2023-06-13 13:47:59 -0400

    connected to database and server started

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Welcome To The 440club-Social-API')
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+const DB = process.env.MONGODB_URI;
+
+const startUp = () => {
+    try {
+        mongoose.connect(DB);
+        app.listen(port, ()=> {
+            console.log(`440club server running on port ${port}...\nConnected to 440club Members Database`)
+        })
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+startUp();
